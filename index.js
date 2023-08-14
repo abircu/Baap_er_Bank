969d0d888b4ffe97b5b8e0aa6f229ff5554fc9ef
abircu <mdabircse4873@gmail.com>
2023-08-15 01:15:20 +0600

    input form adding js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+//step-1: add click event handler with the submit button
 document.getElementById("btn-submit").addEventListener("click", function () {
-  console.log("helleosldkl");
+  //  get the email adress inside the email input field
+  // always remember to use .value to get text from an input field.
+  const emeilField = document.getElementById("user-email");
+  const usrEmail = emeilField.value;
+
+  // Step:03 get password inside the password input field.
+  const passworField = document.getElementById("user-password");
+  const userPass = passworField.value;
+
+  //   Danger: do not use this system in client side.
+  // varify email or password
 });
